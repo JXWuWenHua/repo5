bd086f3f6dafc08fd1d9e4301917e939f018c78b
WuWenHua <1161436535@qq.com>
2019-01-18 19:18:33 +0800

    提交了一个工程

---
+++ Git_day01/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @Description: java类作用描述
+ * @Author: wwh
+ * @CreateDate: 2019/1/18 19:13
+ * @Version: 1.0
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
